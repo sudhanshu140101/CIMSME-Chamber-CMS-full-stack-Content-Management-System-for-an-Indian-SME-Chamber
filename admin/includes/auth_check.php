be1cc46ea599ec31808e09e8a5de9d61655c40c9
Sudhanshu <145291638+sudhanshu140101@users.noreply.github.com>
2026-06-05 11:22:12 +0530

    Delete admin directory

--- admin/includes/auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once dirname(__DIR__, 2) . '/includes/bootstrap.php';
-
-$auth = new Auth();
-$auth->requireLogin();
--- admin/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once dirname(__DIR__) . '/includes/bootstrap.php';
-
-$auth = new Auth();
-redirect($auth->isLoggedIn() ? 'dashboard.php' : 'login.php');
