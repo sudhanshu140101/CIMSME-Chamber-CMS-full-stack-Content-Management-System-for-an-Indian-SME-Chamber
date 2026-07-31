c2dca0419c970df1334e16b08e5b74c232781d42
Sudhanshu <145291638+sudhanshu140101@users.noreply.github.com>
2026-07-31 11:59:10 +0530

    Update schema-mysql.sql

--- database/schema-mysql.sql
+++ database/schema-mysql.sql
@@ -30,6 +30,7 @@ CREATE TABLE events (
     title VARCHAR(255) NOT NULL,
     description TEXT,
     event_date DATE NOT NULL,
+
     event_time VARCHAR(50) DEFAULT '09:00 AM',
     location VARCHAR(255),
     event_type VARCHAR(100) DEFAULT 'conference',
